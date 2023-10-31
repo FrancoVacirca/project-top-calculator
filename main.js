44dbe7faeccd0f7014a82d4a71b87ee55e93cb43
Franco <131470535+FrancoVacirca@users.noreply.github.com>
2023-10-31 14:03:49 -0300

    ui update

---
+++ main.js
@@ -0,0 +1,40 @@
+let inputNumberOne;
+let operator;
+let inputNumberTwo;
+
+const add = () => a + b;
+
+const subtract = () => a - b;
+
+const multiply = () => a * b;
+
+const divide = () => a / b;
+
+const a = 10;
+const b = 5;
+
+//Create a new function operate that takes an operator and 2 numbers and then calls one of the above functions on the numbers.
+function operate() {
+  //const operator = prompt("choose between: add, subtract, multiply and divide");
+
+  switch (operator) {
+    case "add":
+      operator == "add";
+      console.log(add(a, b));
+      break;
+    case "subtract":
+      operator == "subtract";
+      console.log(subtract(a, b));
+      break;
+    case "multiply":
+      operator == "multiply";
+      console.log(multiply(a, b));
+      break;
+    case "divide":
+      operator == "divide";
+      console.log(divide(a, b));
+      break;
+  }
+}
+
+operate();
