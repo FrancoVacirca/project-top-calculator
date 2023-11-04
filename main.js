45e78ee8525bbdb1b8d1f3e102a774d62921342e
Franco <131470535+FrancoVacirca@users.noreply.github.com>
2023-11-03 23:19:20 -0300

    responsive-calc update

--- main.js
+++ main.js
@@ -1,49 +1,68 @@
 const buttons = Array.from(document.querySelectorAll(".button"));
 let display = document.getElementById("display");
+let displayValue = [];
 
-buttons.map(button => {
-  button.addEventListener('click', (e) => {
-    switch(e.target.innerText) {
+buttons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    switch (e.target.innerText) {
       default:
         display.innerHTML += e.target.innerText;
+        displayValue.push(e.target.innerText);
         break;
-      case 'C':
-        display.innerHTML = '';
+      case "C":
+        display.innerHTML = "";
+        displayValue = [];
+        break;
+      case "=":
+        operate();
+        break;
     }
-  })
-})
+  });
+});
 
-const add = () => a + b;
-
-const subtract = () => a - b;
-
-const multiply = () => a * b;
-
-const divide = () => a / b;
-
-const a = 10;
-const b = 5;
-
-//Create a new function operate that takes an operator and 2 numbers and then calls one of the above functions on the numbers.
 function operate() {
-  //const operator = prompt("choose between: add, subtract, multiply and divide");
+  // Join the displayValue array to form an expression
+  const expression = displayValue.join('');
 
-  switch (operator) {
-    case "add":
-      operator == "add";
-      console.log(add(a, b));
-      break;
-    case "subtract":
-      operator == "subtract";
-      console.log(subtract(a, b));
-      break;
-    case "multiply":
-      operator == "multiply";
-      console.log(multiply(a, b));
-      break;
-    case "divide":
-      operator == "divide";
-      console.log(divide(a, b));
-      break;
+  // Use regular expressions to split the expression into operands and operator
+  const regex = /(\d+)([+\-*/])(\d+)/;
+  const match = expression.match(regex);
+
+  if (match) {
+    const operand1 = parseFloat(match[1]);
+    const operator = match[2];
+    const operand2 = parseFloat(match[3]);
+
+    let result;
+
+    // Perform the calculation based on the operator
+    switch (operator) {
+      case '+':
+        result = operand1 + operand2;
+        break;
+      case '-':
+        result = operand1 - operand2;
+        break;
+      case '*':
+        result = operand1 * operand2;
+        break;
+      case '/':
+        if (operand2 !== 0) {
+          result = operand1 / operand2;
+        } else {
+          result = "Error: Division by zero";
+        }
+        break;
+      default:
+        result = "Error: Invalid operator";
+    }
+
+    // Display the result
+    display.innerHTML = result;
+
+    // Clear the displayValue array and add the result as the new starting value
+    displayValue = [result.toString()];
   }
 }
+
+
